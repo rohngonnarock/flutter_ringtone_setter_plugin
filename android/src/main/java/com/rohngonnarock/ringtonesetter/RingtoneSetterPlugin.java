934b93a2c8225d6e2880f84dc500a589a16a3e86
Rahul Prajapati <rahpraja@publicisgroupe.net>
2019-02-25 16:40:45 +0530

    write setting permission added

--- android/src/main/java/com/rohngonnarock/ringtonesetter/RingtoneSetterPlugin.java
+++ android/src/main/java/com/rohngonnarock/ringtonesetter/RingtoneSetterPlugin.java
@@ -1,8 +1,25 @@
 package com.rohngonnarock.ringtonesetter;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
+
+import android.content.ContentValues;
+import android.content.res.AssetManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import android.provider.Settings;
+import android.content.Intent;
 
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -15,6 +32,7 @@ public class RingtoneSetterPlugin implements MethodCallHandler {
   private RingtoneSetterPlugin(Registrar registrar){
     this.ringtone = RingtoneManager.getRingtone(registrar.context().getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
     this.ringtone.setStreamType(AudioManager.STREAM_ALARM);
+    this.registrar = registrar;
   }
 
   /** Plugin registration. */
@@ -30,7 +48,8 @@ public class RingtoneSetterPlugin implements MethodCallHandler {
 		switch (call.method) {
 			case "play":
 			{
-				ringtone.play();
+        //ringtone.play();
+        echo("sdfs","dsfdsf","sdfsf","sdfdsfs",registrar);
 				result.success(null);
 			}
 			break;
@@ -48,67 +67,70 @@ public class RingtoneSetterPlugin implements MethodCallHandler {
 			}
 		}
   }
+
+  private void echo(final String file, final String title, final String artist, 
+  final String tipo, Registrar registrar) {
+		if (!Settings.System.canWrite(registrar.context().getApplicationContext())) {
+			Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+			intent.setData(Uri.parse("package:com.rohngonnarock.ringtonesetter"));
+			registrar.context().startActivity(intent);
+		}
+
+		if (tipo.equals("alarm") || tipo.equals("notification") || tipo.equals("ringtone")) {
+			//setAssets(tipo, file, title, artist, registrar);
+		} else {
+			System.out.println("tipo " + tipo + "not valid");
+		}
+  }
+
+
+	private void setAssets(String type, String file, String title, String artist, Registrar registrar) {
+		File soundFile = new File(file.replaceAll("file:", ""));
+		setRingtone(type, soundFile, title, artist, registrar);
+	}
+
+	private void setRingtone(String type, File newSoundFile, String title, String artist, Registrar registrar) {
+		if (artist.equals("")) {
+			artist = registrar.context().getPackageManager().getApplicationLabel(
+					registrar.context().getApplicationInfo()).toString();
+		}
+		ContentValues values = new ContentValues();
+		values.put(MediaStore.MediaColumns.DATA, newSoundFile.getAbsolutePath());
+		values.put(MediaStore.MediaColumns.TITLE, title.replaceAll("[^\\\\dA-Za-z0-9_]", ""));
+		String filenameArray[] = newSoundFile.getName().split("\\.");
+		values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/" + filenameArray[filenameArray.length - 1]);
+		values.put(MediaStore.MediaColumns.SIZE, newSoundFile.length());
+		values.put(MediaStore.Audio.Media.ARTIST, artist.replaceAll("[^\\\\dA-Za-z0-9_]", ""));
+		values.put(MediaStore.Audio.Media.IS_RINGTONE, true);
+		values.put(MediaStore.Audio.Media.IS_NOTIFICATION, true);
+		values.put(MediaStore.Audio.Media.IS_ALARM, true);
+		values.put(MediaStore.Audio.Media.IS_MUSIC, false);
+
+		Uri uri = MediaStore.Audio.Media.getContentUriForPath(newSoundFile.getAbsolutePath());
+		registrar.context().getContentResolver().delete(uri, MediaStore.MediaColumns.DATA + "=\"" + newSoundFile.getAbsolutePath() + "\"", null);
+		Uri newUri = registrar.context().getContentResolver().insert(uri, values);
+		try {
+			if (type.equals("alarm")) {
+				RingtoneManager.setActualDefaultRingtoneUri(
+						registrar.context(), RingtoneManager.TYPE_ALARM, newUri);
+				//callbackContext.success("success setting " + newSoundFile.getAbsolutePath() + " as " + type);
+			} else if (type.equals("notification")) {
+				RingtoneManager.setActualDefaultRingtoneUri(
+						registrar.context(), RingtoneManager.TYPE_NOTIFICATION, newUri);
+				//callbackContext.success("success setting " + newSoundFile.getAbsolutePath() + " as " + type);
+			} else if (type.equals("ringtone")) {
+				RingtoneManager.setActualDefaultRingtoneUri(
+						registrar.context(), RingtoneManager.TYPE_RINGTONE, newUri);
+				//callbackContext.success("success setting " + newSoundFile.getAbsolutePath() + " as " + type);
+			} else {
+				//callbackContext.error("tipo " + type + "not valid");
+			}
+		} catch(Throwable t) {
+			Log.d("tag", "catch exception");
+			//callbackContext.error("Error setting as " + type + t.getMessage() + ". Destino: " + newSoundFile.getPath());
+		}
+  }
   
   private final Ringtone ringtone;
-
-	// private boolean echo(final String file, final String title, final String artist, final String tipo, final CallbackContext callbackContext) {
-	// 	if (!Settings.System.canWrite(cordova.getActivity().getApplicationContext())) {
-	// 		Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
-	// 		intent.setData(Uri.parse("package:io.ionic.starter"));
-	// 		cordova.getActivity().startActivity(intent);
-	// 	}
-
-	// 	if (tipo.equals("alarm") || tipo.equals("notification") || tipo.equals("ringtone")) {
-	// 		setAssets(tipo, file, title, artist, callbackContext);
-	// 	} else {
-	// 		callbackContext.error("tipo " + tipo + "not valid");
-	// 	}
-	// }
-
-	// private void setAssets(String type, String file, String title, String artist, CallbackContext callbackContext) {
-	// 	File soundFile = new File(file.replaceAll("file:", ""));
-	// 	setRingtone(type, soundFile, callbackContext, title, artist);
-	// }
-
-	// private void setRingtone(String type, File newSoundFile, CallbackContext callbackContext, String title, String artist) {
-	// 	if (artist.equals("")) {
-	// 		artist = cordova.getActivity().getPackageManager().getApplicationLabel(
-	// 				cordova.getActivity().getApplicationInfo()).toString();
-	// 	}
-	// 	ContentValues values = new ContentValues();
-	// 	values.put(MediaStore.MediaColumns.DATA, newSoundFile.getAbsolutePath());
-	// 	values.put(MediaStore.MediaColumns.TITLE, title.replaceAll("[^\\\\dA-Za-z0-9_]", ""));
-	// 	String filenameArray[] = newSoundFile.getName().split("\\.");
-	// 	values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/" + filenameArray[filenameArray.length - 1]);
-	// 	values.put(MediaStore.MediaColumns.SIZE, newSoundFile.length());
-	// 	values.put(MediaStore.Audio.Media.ARTIST, artist.replaceAll("[^\\\\dA-Za-z0-9_]", ""));
-	// 	values.put(MediaStore.Audio.Media.IS_RINGTONE, true);
-	// 	values.put(MediaStore.Audio.Media.IS_NOTIFICATION, true);
-	// 	values.put(MediaStore.Audio.Media.IS_ALARM, true);
-	// 	values.put(MediaStore.Audio.Media.IS_MUSIC, false);
-
-	// 	Uri uri = MediaStore.Audio.Media.getContentUriForPath(newSoundFile.getAbsolutePath());
-	// 	cordova.getActivity().getContentResolver().delete(uri, MediaStore.MediaColumns.DATA + "=\"" + newSoundFile.getAbsolutePath() + "\"", null);
-	// 	Uri newUri = cordova.getActivity().getContentResolver().insert(uri, values);
-	// 	try {
-	// 		if (type.equals("alarm")) {
-	// 			RingtoneManager.setActualDefaultRingtoneUri(
-	// 					cordova.getActivity(), RingtoneManager.TYPE_ALARM, newUri);
-	// 			callbackContext.success("success setting " + newSoundFile.getAbsolutePath() + " as " + type);
-	// 		} else if (type.equals("notification")) {
-	// 			RingtoneManager.setActualDefaultRingtoneUri(
-	// 					cordova.getActivity(), RingtoneManager.TYPE_NOTIFICATION, newUri);
-	// 			callbackContext.success("success setting " + newSoundFile.getAbsolutePath() + " as " + type);
-	// 		} else if (type.equals("ringtone")) {
-	// 			RingtoneManager.setActualDefaultRingtoneUri(
-	// 					cordova.getActivity(), RingtoneManager.TYPE_RINGTONE, newUri);
-	// 			callbackContext.success("success setting " + newSoundFile.getAbsolutePath() + " as " + type);
-	// 		} else {
-	// 			callbackContext.error("tipo " + type + "not valid");
-	// 		}
-	// 	} catch(Throwable t) {
-	// 		Log.d("tag", "catch exception");
-	// 		callbackContext.error("Error setting as " + type + t.getMessage() + ". Destino: " + newSoundFile.getPath());
-	// 	}
-	// }
+  private final Registrar registrar;
 }
